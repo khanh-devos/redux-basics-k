a50ebfc591f56f962b01b0a814a3503ce7be1202
khanh-devos <dreamproperty.khanh@gmail.com>
2023-07-24 20:43:40 +0700

    add redux with a store, 2 reducers, and 1 extraReducers for managing fetching an API

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
+import CartContainer from './components/CartContainer';
+import Modal from './components/Modal';
+import Navbar from './components/Navbar';
+import { calculateTotals, getCartItems } from './features/cart/cartSlice';
 
 function App() {
+  const { cartItems, isLoading } = useSelector((store) => store.cart);
+  const { isOpen } = useSelector((store) => store.modal);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems]);
+
+  useEffect(() => {
+    dispatch(getCartItems('random'));
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className="loading">
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
+
   return (
-    <div className="App" />
+    <div className="App">
+      {isOpen && <Modal />}
+      <Navbar />
+      <CartContainer />
+    </div>
   );
 }
 
